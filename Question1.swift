50236299071a81824c622bc8f39cd921fc52f274
Hank Wang <zxbjwh@126.com>
2017-07-24 20:00:49 +0800

    Update Question1.swift

--- Question1.swift
+++ Question1.swift
@@ -1,12 +1,10 @@
 //
-//  Homework.swift
-//  
+//
+//
 //
 //  Created by Hank on 2017/7/24.
 //
 //
-import UIKit
-import Foundation
 
 func paidrome(string:String)->Bool{
     var count = 0
@@ -14,13 +12,13 @@ func paidrome(string:String)->Bool{
     for _ in characters{
         count = count+1
     }
-    for i in 1...characters.count{
-        guard characters[i-1] == characters[count-i] else{
+    for i in 1...count/2{
+        if characters[i-1] == characters[count-i] {
+        }
+        else{
             return false
         }
     }
     return true
 }
-
-
 paidrome(string: "racecar")
