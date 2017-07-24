3fa337cdc5d4c9262e0e55febcd36555a0161bb0
Hank Wang <zxbjwh@126.com>
2017-07-24 15:51:16 +0800

    Add files via upload

--- Question1.swift
+++ Question1.swift
@@ -5,17 +5,22 @@
 //  Created by Hank on 2017/7/24.
 //
 //
-
+import UIKit
 import Foundation
 
 func paidrome(string:String)->Bool{
+    var count = 0
     let characters = Array(string.characters)
+    for _ in characters{
+        count = count+1
+    }
     for i in 1...characters.count{
-        guard characters[i-1] == characters[characters.count-i] else{
+        guard characters[i-1] == characters[count-i] else{
             return false
         }
     }
     return true
 }
+
 
 paidrome(string: "racecar")
